b87d0652596e8e63d2850746aa9ae28a409d6e90
studiomac-152 <studiomac-152@studiomac-152s-iMac.local>
2022-09-12 18:11:22 +0530

    start creating animations with the id.

---
+++ script.js
@@ -0,0 +1,15 @@
+// // const btnNavEl = document.querySelector(".c-menu-button .w-inline-block");
+// // const headerEl = document.querySelector(".c-navbar .w-nav");
+
+// // btnNavEl.addEventListener("click", function () {
+// //   headerEl.classList.toggle(".c-nav__menu-list");
+// // });
+
+
+// const btn = document.getElementById('no-time-mobile');
+
+// btn.addEventListener('click', function onClick(event) {
+//   document.body.style.backgroundColor = 'aqua';
+// });
+
+
