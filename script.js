3850b93a01b6fd43f3a99c457bc0c9f52a5a3577
studiomac-152 <studiomac-152@studiomac-152s-iMac.local>
2022-10-11 18:35:14 +0530

    Apply atomic files on the website.

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-// // const btnNavEl = document.querySelector(".c-menu-button .w-inline-block");
-// // const headerEl = document.querySelector(".c-navbar .w-nav");
-
-// // btnNavEl.addEventListener("click", function () {
-// //   headerEl.classList.toggle(".c-nav__menu-list");
-// // });
-
-
-// const btn = document.getElementById('no-time-mobile');
-
-// btn.addEventListener('click', function onClick(event) {
-//   document.body.style.backgroundColor = 'aqua';
-// });
-
-// function redirect() {
-//   let url = "/Attract";
-//   window.location(url);
-// }
-
-document.getElementById("redirect-page").onclick = function () {
-  location.href = "/solutions/attract";
-};
-
+const nav = document.querySelector(".navigationBar")
+fetch("/src/components/organisms/navbar/navbar.html")
+  .then(res => res.text())
+  .then(data => {
+    nav.innerHTML = data
+  })
